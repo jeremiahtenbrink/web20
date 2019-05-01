b4b09757258e4825664b74228fe64114d911b3e7
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-05-01 13:18:06 -0600

    Started working on student form page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch, NavLink } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import { checkAuth, getStudents, getUser } from "./actions";
 
