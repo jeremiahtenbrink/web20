6f78186bd144a5b705fa5a6684dccc84d140f0d2
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-06-06 11:45:53 -0600

    Changed some folder names around to be able to compile.

--- src/views/Attendance.js
+++ src/views/Attendance.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import AttendanceReport from "../oldcomponents/attendanceReport/AttendanceReport";
+import AttendanceReport from "../components/attendance/AttendanceReport";
 
 const Attendance = props => {
   return (
