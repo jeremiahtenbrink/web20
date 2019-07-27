998998ffa764bd5390ed5a816e3cc3d0eb1530f5
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-07-27 11:14:35 -0600

    Changed Project Manger to Team Lead in the sprint form.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    apiiId: process.env.REACT_APP_APP_ID
+    apiId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
